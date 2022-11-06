686980db146fcd46ab5779b1e1ac51832504a0ec
myankpraksh <myankpraksh@gmail.com>
2022-11-06 16:47:41 +0530

    rating addedin cardcomponent

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -7,21 +7,16 @@ export default function CardContainer({ messArray, pin }) {
       {messArray.length > 0 ? (
         <div className="CardContainer mb4 pv5 shadow-5">
           <section className="mw7 center">
-            <h2 className=" ph3 ph0-l tc f2">
-              Mess found at pincode - {pin}
-            </h2>
+            <h2 className=" ph3 ph0-l tc f2">Mess found at pincode - {pin}</h2>
             {messArray.map((mess) => {
               return (
-                
                 <article
                   className="pv4 bt bb b--black-10 ph3 ph0-l pointer near-black"
                   key={mess.id}
                 >
                   <div className="flex flex-column flex-row-ns">
                     <div className="w-100 w-60-ns pr3-ns order-2 order-1-ns">
-                      <h1 className="f3  mt0 lh-title ttc">
-                        {mess.name}
-                      </h1>
+                      <h1 className="f3  mt0 lh-title ttc">{mess.name}</h1>
                       <p className="f5 f4-l lh-copy ">
                         {mess.short_description}
                       </p>
@@ -35,8 +30,24 @@ export default function CardContainer({ messArray, pin }) {
                     <p>{mess.address}</p>
                     <p>{"Phone no. " + mess.phone}</p>
                   </div>
+                  <div className="pt2 ttc">
+                    <p className="b pb2 di">Rating - </p>
+                    <p className="di">{mess.rating + " ⭐/5 ⭐"}</p>
+                  </div>
+                  <div className="pt2 ttc">
+                    <label for="messrating" className="b pb2">
+                      Rate this mess -{" "}
+                    </label>
+                    <select name="messrating" id="messrating">
+                      <option value="1">1 ⭐</option>
+                      <option value="2">2 ⭐</option>
+                      <option value="3">3 ⭐</option>
+                      <option value="4">4 ⭐</option>
+                      <option value="4">5 ⭐</option>
+                    </select>
+                    <input type="submit" className="ma2" value="Rate it!" />
+                  </div>
                 </article>
-                
               );
             })}
           </section>
